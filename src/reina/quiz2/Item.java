88d3f5261c920ec3eeb133bcd1c8a7398bbeb473
Reina Trycha Dani <trycha67@gmail.com>
2018-11-26 18:20:20 +0700

    menambahkan atribut di class Item

---
+++ src/reina/quiz2/Item.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reina.quiz2;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Item {
+    private String nama; //variabel nama untuk nama item
+    private float harga; //variabel harga untuk harga item
+    private int jumlah; //variabel jumlah untuk jumlah item
+}
