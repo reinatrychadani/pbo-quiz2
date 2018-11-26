aaa8477e0e43b90281dc3681284bfec723dc83b7
Reina Trycha Dani <trycha67@gmail.com>
2018-11-26 18:22:24 +0700

    menambahkan konstruktor default dan konstruktor berparameter di class Item

--- src/reina/quiz2/Item.java
+++ src/reina/quiz2/Item.java
@@ -13,4 +13,16 @@ public class Item {
     private String nama; //variabel nama untuk nama item
     private float harga; //variabel harga untuk harga item
     private int jumlah; //variabel jumlah untuk jumlah item
+
+    //membuat konstruktor default
+    public Item() {
+    }
+    //membuat konstruktor berparameter
+    public Item(String nama, float harga, int jumlah) {
+        this.nama = nama;
+        this.harga = harga;
+        this.jumlah = jumlah;
+    }
+    
+    
 }
