3e3cae8a05bf82e40a68ff5fb2558d4a36372bc1
Reina Trycha Dani <trycha67@gmail.com>
2018-11-28 17:22:04 +0700

    memperbaiki konstruktor di class Transaksi

--- src/reina/quiz2/Transaksi.java
+++ src/reina/quiz2/Transaksi.java
@@ -17,9 +17,9 @@ public class Transaksi {
     private float total; //variabel total untuk menyimpan total
 
     //membuat konstruktor berparameter
-    public Transaksi(String code, float total) {
+    public Transaksi(String code, ArrayList<Item> items) {
         this.code = code;
-        this.total = total;
+        this.items = items;
     }
     
     //setter total
